07f8cc2f26616cb7eed239b3d03650100a2a4e25
Samuel Maturo <samuel.maturo@gmail.com>
2018-10-19 01:23:34 +0000

    Starting

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
-
-//GLOBAL VARIABLES
-/* global $ */
-
-
-
-// DOCUMENT READY FUNCTION BELOW
-
+/*global $*/
+$(document).ready(function(){
+    $("#verify").click(function(){
+        var one1 = $("#number1").val();
+        var one2 = $("#number2").val();
+        var one3 = $("#number3").val();
+        var one4 = $("#number4").val();
+        $("#userChoice").text(one1 + " " + one2 + " " + one3 + " " + one4);
+    });
+    var solOne1 = Math.floor(Math.random() * 10);
+    var solOne2 = Math.floor(Math.random() * 10);
+    var solOne3 = Math.floor(Math.random() * 10);
+    var solOne4 = Math.floor(Math.random() * 10);
+    if (one1 === solOne1){
+        $("#number1").css("background-color, green");
+    }
+});
