9c1307a2d5afc38fb82d0065a22340dcdada36aa
Samuel Maturo <samuel.maturo@gmail.com>
2018-11-02 01:20:45 +0000

    Final

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 /*global $*/
 $(document).ready(function(){
+    $("#two").hide();
+    $("#open").hide();
     let solOne1 = Math.floor(Math.random() * 10);
     let solOne2 = Math.floor(Math.random() * 10);
     let solOne3 = Math.floor(Math.random() * 10);
@@ -14,7 +16,6 @@ $(document).ready(function(){
         let one2 = $("#number2").val();
         let one3 = $("#number3").val();
         let one4 = $("#number4").val();
-        $("#userChoice").text(one1 + " " + one2 + " " + one3 + " " + one4);
     if (one1.toString() === solOne1.toString()){
         $("#number1").css("background-color", "green");
         solOne1s = true;
@@ -79,11 +80,99 @@ $(document).ready(function(){
     else {
         $("#number4").css("background-color", "red");
     }
-    if (solOne1s === true, solOne2s === true, solOne3s === true, solOne4s === true){
+    if (solOne1s === true && solOne2s === true && solOne3s === true && solOne4s === true){
         oneSolved === true;
         $("#results").html("You got the first one! Now for number 2!");
+        $("#closed").hide();
+        $("#open").show();
+        $("#two").show();
+        $("#open2").hide();
+    }
+    });
+    $("#open").hide();
+    let solTwo1 = Math.floor(Math.random() * 10);
+    let solTwo2 = Math.floor(Math.random() * 10);
+    let solTwo3 = Math.floor(Math.random() * 10);
+    let solTwo4 = Math.floor(Math.random() * 10);
+    let solTwo1s = false;
+    let solTwo2s = false;
+    let solTwo3s = false;
+    let solTwo4s = false;
+    let twoSolved = false;
+    $("#verify2").click(function(){
+        let two1 = $("#numberTwo1").val();
+        let two2 = $("#numberTwo2").val();
+        let two3 = $("#numberTwo3").val();
+        let two4 = $("#numberTwo4").val();
+    if (two1.toString() === solTwo1.toString()){
+        $("#numberTwo1").css("background-color", "green");
+        solTwo1s = true;
+    } 
+    else if (two1.toString() === solTwo2.toString()){
+        $("#numberTwo1").css("background-color", "yellow");
+    }
+    else if (two1.toString() === solTwo3.toString()){
+        $("#numberTwo1").css("background-color", "yellow");
+    }
+    else if (two1.toString() === solTwo4.toString()){
+        $("#numberTwo1").css("background-color", "yellow");
+    }
+    else {
+        $("#numberTwo1").css("background-color", "red");
+    }
+    if (two2.toString() === solTwo2.toString()){
+        $("#numberTwo2").css("background-color", "green");
+        solTwo2s = true;
+    }
+    else if (two2.toString() === solTwo1.toString()){
+        $("#numberTwo2").css("background-color", "yellow");
+    }
+    else if (two2.toString() === solTwo3.toString()){
+        $("#numberTwo2").css("background-color", "yellow");
+    }
+    else if (two2.toString() === solTwo4.toString()){
+        $("#numberTwo2").css("background-color", "yellow");
+    }
+    else {
+        $("#numberTwo2").css("background-color", "red");
+    }
+    if (two3.toString() === solTwo3.toString()){
+        $("#numberTwo3").css("background-color", "green");
+        solTwo3s = true;
+    }
+    else if (two3.toString() === solTwo1.toString()){
+        $("#numberTwo3").css("background-color", "yellow");
+    }
+    else if (two3.toString() === solTwo2.toString()){
+        $("#numberTwo3").css("background-color", "yellow");
+    }
+    else if (two3.toString() === solTwo4.toString()){
+        $("#numberTwo3").css("background-color", "yellow");
+    }
+    else {
+        $("#numberTwo3").css("background-color", "red");
+    }
+    if (two4.toString() === solTwo4.toString()){
+        $("#numberTwo4").css("background-color", "green");
+        solTwo4s = true;
+    }
+    else if (two4.toString() === solTwo1.toString()){
+        $("#numberTwo4").css("background-color", "yellow");
+    }
+    else if (two4.toString() === solTwo2.toString()){
+        $("#numberTwo4").css("background-color", "yellow");
+    }
+    else if (two4.toString() === solTwo3.toString()){
+        $("#numberTwo4").css("background-color", "yellow");
+    }
+    else {
+        $("#numberTwo4").css("background-color", "red");
+    }
+    if (solTwo1s === true && solTwo2s === true && solTwo3s === true && solTwo4s === true){
+        twoSolved === true;
+        $("#results2").html("yay u did it again lol");
+        $("#closed2").hide();
+        $("#open2").show();
     }
     });
 });
-function compare (i, x){
-}
